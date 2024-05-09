393d422027c0f81e21e4a57d13ef9c26a5b6781c
LKubrick <lorenzo.carly@protonmail.Com>
2024-05-09 17:52:25 +0200

    Removed Parent Constraint and updated MiniController
    Updated MiniatureObjectController.cs so it's applying the initialPosition and rotation at the end of each FixedUpdate loop.
    Applied a factor of 8 to have the movement translate in real space
    used the basic grabbable instead of building block for simplicity

--- Assets/MiniatureObjectController.cs
+++ Assets/MiniatureObjectController.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 public class MiniatureObjectController : MonoBehaviour
 {
     private Vector3 initialPosition;
+    private Quaternion initialRotation;
     public Transform lifeSizeObject; // Assign this in the Inspector
+    public float multiplyFactor = 2f;
 
     private void Start()
     {
         // Store the initial position of this object
         initialPosition = transform.position;
+        initialRotation = transform.rotation;
     }
 
     private void FixedUpdate()
     {
         // Calculate the delta (current position - initial position)
         Vector3 delta = transform.position - initialPosition;
+        Quaternion deltaRot = transform.rotation * Quaternion.Inverse(initialRotation);
 
         // Apply the delta to the corresponding life-size object
-        lifeSizeObject.position += delta;
+        lifeSizeObject.position += (delta * multiplyFactor);
+        initialPosition = transform.position;
+        lifeSizeObject.rotation = initialRotation * deltaRot;
+        initialRotation = transform.rotation;
     }
 }
