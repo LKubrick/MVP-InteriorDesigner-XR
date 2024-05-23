f5cd3118801d2d407ef4dd738d2e79677b299f4b
isaacyho <isaacyho@users.noreply.github.com>
2024-05-23 18:58:57 -0300

    rotation works!

--- Assets/MiniatureObjectController.cs
+++ Assets/MiniatureObjectController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class MiniatureObjectController : MonoBehaviour
 {
     private Vector3 initialPosition;
-    private Quaternion initialRotation;
     public Transform lifeSizeObject; // Assign this in the Inspector
     public float multiplyFactor = 8f;
 
@@ -11,25 +10,21 @@ public class MiniatureObjectController : MonoBehaviour
     {
         // Store the initial position of this object
         initialPosition = transform.localPosition;
-        initialRotation = transform.localRotation;
     }
 
     private void FixedUpdate()
     {
         // Calculate the delta (current position - initial position)
         Vector3 delta = transform.localPosition - initialPosition;
-        Quaternion deltaRot = transform.localRotation * Quaternion.Inverse(initialRotation);
         initialPosition = transform.localPosition;
-        initialRotation = transform.localRotation;
 
-        // XXX check for rotation too
         if (delta.magnitude > 0.001f)
         {
             // Apply the delta to the corresponding life-size object
             if (lifeSizeObject)
             {
                 lifeSizeObject.localPosition += (delta * multiplyFactor);
-                lifeSizeObject.localRotation = initialRotation * deltaRot;
+                lifeSizeObject.localRotation = transform.localRotation;
             }
 
         }
